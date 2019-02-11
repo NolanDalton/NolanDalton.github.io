d58aadd99694a27e634ced02821f29e6300cdf1a
Aj Romaniello <ajromaniello@AJs-MacBook-Air.local>
2019-02-10 23:41:23 -0500

    Added all NORMAL Fire Heroes, and began array list including images in summon.js

--- Summon.js
+++ Summon.js
@@ -8,6 +8,40 @@ var elementalNature = summonType[4];
 var elementalHoly = summonType[5]
 var elementalDark = summonType[6];
 
+//image array for the fire heroes (0-3 = 3*, 4-8 = 4*, 14-24 = 5*) These are ALL fire heroes
+var fireHeroes = [];
+//Azar
+fireHeroes[0] = new Image();
+fireHeroes[0].src = 'misc/fire-heroes/azar.png';
+//Hawkmoon
+fireHeroes[1] = new Image();
+fireHeroes[1].src = 'misc/fire-heroes/hawkmoon.png';
+//Jahangir
+fireHeroes[2] = new Image();
+fireHeroes[2].src = 'misc/fire-heroes/jahangir.png';
+//Nashgar
+fireHeroes[3] = new Image();
+fireHeroes[3].src = 'misc/fire-heroes/nashgar.png';
+//Boldtusk
+fireHeroes[4] = new Image();
+fireHeroes[4].src = 'misc/fire-heroes/boldtusk.png';
+//Colen
+fireHeroes[5] = new Image();
+fireHeroes[5].src = 'misc/fire-heroes/colen.png';
+//Gormek
+fireHeroes[6] = new Image();
+fireHeroes[6].src = 'misc/fire-heroes/gormek.png';
+//Kelile
+fireHeroes[7] = new Image();
+fireHeroes[7].src = 'misc/fire-heroes/kelile.png';
+//Scarlett
+fireHeroes[8] = new Image();
+fireHeroes[8].src = 'misc/fire-heroes/scarlett.png';
+
+//ADD MORE USING FORMAT
+fireHeroes[9] = new Image();
+fireHeroes[9].src = 'misc/fire-heroes/HERO.png';
+
 
 
 function getSummonType() {
@@ -32,3 +66,4 @@ function summonEvent() {
 
 
 }
+
