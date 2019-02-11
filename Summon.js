679c6d43a51d797bd54963c314043a4b03ea081f
ajrom47 <beastlyaj01@gmail.com>
2019-02-10 19:46:21 -0500

    Added Summon.JS
    ;)

---
+++ Summon.js
@@ -0,0 +1,34 @@
+// JavaScript source code
+var summonTypes = ["Epic Hero", "Epic Troop", "Elemental Fire", "Elemental Ice", "Elemental Nature", "Elemental Holy", "Elemental Dark"];
+var epicHero = summonType[0];
+var epicTroop = summonType[1];
+var elementalFire = summonType[2];
+var elementalIce = summonType[3];
+var elementalNature = summonType[4];
+var elementalHoly = summonType[5]
+var elementalDark = summonType[6];
+
+
+
+function getSummonType() {
+
+}
+
+function summonEpicHero() {
+
+
+}
+
+function summonEpicTroop() {
+
+
+}
+
+function summonElemental() {
+
+
+}
+function summonEvent() {
+
+
+}
